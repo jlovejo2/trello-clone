07d336062cf4991cce411d869b8b9fc20c7169e2
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-06-10 08:52:38 -0400

    added addNewItem componnent to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AddNewItem } from "./AddNewItem"
 import { Column } from "./Column"
 import { Appcontainer } from "./styles"
 
@@ -5,6 +6,7 @@ export const App = () => {
   return (
     <Appcontainer>
       <Column text='Todo' />
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </Appcontainer>
   )
 }
