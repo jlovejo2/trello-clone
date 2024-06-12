980e4500ee164518db86187a3c3ada84f38b1c25
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-06-11 21:15:38 -0400

    update components to use the context and state hook:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { AddNewItem } from "./AddNewItem"
 import { Column } from "./Column"
+import { useAppState } from "./state/AppStateContext"
 import { Appcontainer } from "./styles"
 
 export const App = () => {
+  const { lists } = useAppState();
+
   return (
     <Appcontainer>
-      <Column text='Todo' />
+      {lists.map((list): any => {
+        return <Column text={list.text} key={list.id} id={list.id} />
+      })}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </Appcontainer>
   )
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,24 @@
 import { AddNewItem } from "./AddNewItem"
 import { Card } from "./Card"
+import { useAppState } from "./state/AppStateContext"
 import { ColumnContainer, ColumnTitle } from "./styles"
 
 type ColumnProps = {
     text: string
+    id: string
 }
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ text, id }: ColumnProps) => {
+    const { getTasksByListId } = useAppState();
+
+    const tasks = getTasksByListId(id);
+
     return (
         <ColumnContainer>
             <ColumnTitle> { text } </ColumnTitle>
-            <Card text='Generate App Scafold'/>
-            <Card text='Learn Typescript' />
-            <Card text='Begin to use static typing' />
+            {tasks.map((task): any => {
+                return <Card text={task.text} key={task.id} id={task.id} />
+            })}
             <AddNewItem 
                 toggleButtonText="+ Add another card"
                 onAdd={console.log}
