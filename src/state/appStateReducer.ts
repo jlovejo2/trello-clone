a60b1a06a4aae91b5a0ec5ad74ae13c916c146b2
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-06-12 21:53:45 -0400

    add use-immer, action.ts, and appstatreducer

---
+++ src/state/appStateReducer.ts
@@ -0,0 +1,24 @@
+import { Action } from './actions';
+
+export type Task = {
+    id: string
+    text: string
+}
+
+export type List = {
+    id: string
+    text: string
+}
+
+export type AppState = {
+    lists: List[]
+}
+
+export const appStateReducer = (
+    draft: AppState,
+    action: Action,
+): AppState | void => {
+    switch (action.type) {
+        
+    }
+}
