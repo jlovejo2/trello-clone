0acebb3c4f1cd25b8860f523fde51820975559a6
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-06-19 13:42:07 -0400

    implemented drag and drop to column component

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,21 +1,40 @@
+import { useRef } from "react";
+import { useDrop } from "react-dnd";
 import { AddNewItem } from "./AddNewItem"
 import { Card } from "./Card"
 import { useAppState } from "./state/AppStateContext"
-import { addTask } from "./state/actions"
+import { addTask, moveList } from "./state/actions"
 import { ColumnContainer, ColumnTitle } from "./styles"
+import { useItemDrag } from "./utils/useItemDrag";
+import { throttle } from "./utils/throttle";
 
 type ColumnProps = {
     text: string
     id: string
 }
 
+
 export const Column = ({ text, id }: ColumnProps) => {
-    const { getTasksByListId, dispatch } = useAppState();
+    const { draggedItem, getTasksByListId, dispatch } = useAppState();
+    const ref = useRef<HTMLDivElement>(null);
+    
+    const [, drop] = useDrop({ accept: "COLUMN", hover: throttle(200, () => {
+        if (!draggedItem) return;
+        if (draggedItem.type === "COLUMN") {
+            if (draggedItem.id === id) return;
+        }
+
+        dispatch(moveList(draggedItem.id, id))
+    })})
+
+    const { drag } = useItemDrag({type: "COLUMN", id, text});
 
     const tasks = getTasksByListId(id);
+    
+    drag(drop(ref));
 
     return (
-        <ColumnContainer>
+        <ColumnContainer ref={ref}>
             <ColumnTitle> { text } </ColumnTitle>
             {tasks.map((task): any => {
                 return <Card text={task.text} key={task.id} id={task.id} />
